65f0c3cf21d7b765d637d45c5680594c6bc3e1c9
Juan Felipe Jaramillo <felipe.jaramillo@booktofly.com.co>
2023-06-30 17:18:00 -0500

    Allow all origins was added

--- RickAndMorty.Api/Program.cs
+++ RickAndMorty.Api/Program.cs
@@ -1,6 +1,9 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using RickAndMorty.Application.Services;
-using RickAndMorty.Application.Services.Character;
+using RickAndMorty.Application.Services.Characters;
 using RickAndMorty.Domain.Services;
 using RickAndMorty.Infrastracture;
 using RickAndMorty.Infrastracture.Data;
@@ -26,13 +29,41 @@ builder.Services.AddDbContext<RickAndMortyContext>( context => context.UseMySql(
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors( p => p.AddPolicy( "corsapp", builder =>
+builder.Services.AddAuthentication( options =>
 {
-	builder.WithOrigins( "*" ).AllowAnyMethod().AllowAnyHeader();
-} ) );
+	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+} ).AddJwtBearer( options =>
+{
+	options.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuer = true,
+		ValidateAudience = true,
+		ValidateLifetime = true,
+		ValidateIssuerSigningKey = true,
+		ValidIssuer = "https://yourdomain.com",
+		ValidAudience = "YourAudience",
+		IssuerSigningKey = new SymmetricSecurityKey( Encoding.UTF8.GetBytes( "YourSecretKey" ) )
+	};
+} );
+
+
+
+
+builder.Services.AddCors( options =>
+{
+	options.AddPolicy( "AllowAllOrigins",
+		builder =>
+		{
+			builder.AllowAnyOrigin()
+				   .AllowAnyMethod()
+				   .AllowAnyHeader();
+		} );
+} );
 
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 
 var logger = app.Services.GetRequiredService<ILogger<Program>>();
 logger.LogInformation( "Application starting up..." );
@@ -46,7 +77,8 @@ if( app.Environment.IsDevelopment() )
 
 app.UseHttpsRedirection();
 
-app.UseCors( "corsapp" );
+app.UseCors( "AllowAllOrigins" );
+
 
 app.UseAuthorization();
 
